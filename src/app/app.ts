6fc19702020e113f4886eda79f511142e6d8d7be
Dinesh <rsdineshbaburajkumar@gmail.com>
2026-01-20 21:53:22 +0530

    c

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './styles.css'
 })
-export class App {
-  protected readonly title = signal('hotel-booking-system');
+export class AppComponent {
+  title = 'hotel-booking-system';
 }
