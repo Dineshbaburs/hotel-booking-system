2689c7854c59ca3e248ee4257d6c3a484fde0111
Dinesh <rsdineshbaburajkumar@gmail.com>
2026-01-21 10:02:25 +0530

    c

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,7 @@ import { NavbarComponent } from './components/navbar/navbar';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent], 
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
