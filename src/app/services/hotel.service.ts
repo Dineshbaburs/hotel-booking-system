a3f8bb33d5ba47800b365b53ce345361ed061d4d
Dinesh <rsdineshbaburajkumar@gmail.com>
2026-01-21 12:21:58 +0530

    c

--- src/app/services/hotel.service.ts
+++ src/app/services/hotel.service.ts
@@ -2,26 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Hotel } from '../models/hotel.model';
-import { Room } from '../models/room.model'; // <--- Import Room model
 
 @Injectable({
   providedIn: 'root'
 })
 export class HotelService {
-  private apiUrl = 'http://localhost:3000'; // Base URL
+  private apiUrl = 'http://localhost:3000/hotels';
 
   constructor(private http: HttpClient) {}
 
   getHotels(): Observable<Hotel[]> {
-    return this.http.get<Hotel[]>(`${this.apiUrl}/hotels`);
-  }
-
-  getHotelById(id: number): Observable<Hotel> {
-    return this.http.get<Hotel>(`${this.apiUrl}/hotels/${id}`);
-  }
-
-  // NEW METHOD: Get rooms for a specific hotel
-  getRoomsByHotelId(hotelId: number): Observable<Room[]> {
-    return this.http.get<Room[]>(`${this.apiUrl}/rooms?hotelId=${hotelId}`);
+    return this.http.get<Hotel[]>(this.apiUrl);
   }
 }
